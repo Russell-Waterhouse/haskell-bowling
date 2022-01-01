68115217fb95c3f3f32e596a77f819d1e68b12aa
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2021-12-31 18:00:24 -0800

    started work on the scoring module. This works for regular rolls but does not work with spares or strikes

--- src/Scoring.hs
+++ src/Scoring.hs
@@ -1,6 +1,46 @@
-module Scoring (score) where
+{-# LANGUAGE NoImplicitPrelude #-}
+module Scoring (Roll(..), Frame(..), Game(..), score) where
 
 
+import Import
 
-score :: undefined -> Int 
-score _ = 0
+type Roll =  Int
+
+data Frame = Frame {
+              roll1 :: Roll, 
+              roll2 :: Maybe Roll,
+              roll3 :: Maybe Roll
+            }  
+            | Spare
+            | Strike 
+
+
+type Game =  [Frame]
+
+score :: Game -> Int
+score game | 10 == length game = score' game 0
+score _ | otherwise = 0
+
+score' :: Game -> Int -> Int
+score' [] currentScore =  currentScore
+score' (x: xs) currentScore = 
+  let frameScore = scoreFrame x xs
+      newScore = frameScore + currentScore
+   in score' xs newScore
+score' _ _ | otherwise = 0
+
+scoreFrame :: Frame -> [Frame] -> Int
+scoreFrame (Frame roll1 roll2 roll3) framesAfterRoll = 
+  let roll2Score = case roll2 of
+                     Just r2 -> r2
+                     Nothing -> 0
+      roll3Score = case roll3 of 
+                     Just r3 -> r3 
+                     Nothing -> 0
+  in 
+    roll1 + roll2Score + roll3Score
+scoreFrame Spare framesAfterRoll = undefined
+scoreFrame Strike framesAfterRoll = undefined
+
+addNextRolls:: Game -> Int -> Int
+addNextRolls = undefined
--- test/ScoringSpec.hs
+++ test/ScoringSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module ScoringSpec (spec) where
 
 import Import
@@ -8,10 +9,22 @@ import Test.Hspec.QuickCheck
 
 spec :: Spec
 spec = do
-  let zeros = undefined
+  let zeroRoll :: Roll = 0
+      zeroFrame :: Frame = Frame zeroRoll (Just zeroRoll) Nothing
+      zeroGame :: Game = makeGameOfFrame zeroFrame (Frame zeroRoll (Just zeroRoll) (Just zeroRoll))
+      oneFrame :: Frame = Frame (1) (Just 1) Nothing
+      onesGame :: Game = makeGameOfFrame oneFrame oneFrame
+      strikeFrame :: Frame = Strike
+      perfectGame :: Game = makeGameOfFrame strikeFrame strikeFrame
   describe "score" $ do
-    it "score zeros" $ score zeros `shouldBe` 0
+    it "score invalid Game" $ score [] `shouldBe` 0
+    it "score zeros" $ score zeroGame `shouldBe` 0
+    it "score 1's" $ score onesGame `shouldBe` 20
+   -- it "score perfect game" $ score perfectGame `shouldBe` 300
 
     --it "overflow" $ plus2 maxBound `shouldBe` minBound + 1
     --prop "minus 2" $ \i -> plus2 i - 2 `shouldBe` i
 
+makeGameOfFrame :: Frame -> Frame -> Game
+makeGameOfFrame frame lastFrame = 
+  [frame, frame, frame, frame, frame, frame, frame, frame, frame, lastFrame]
