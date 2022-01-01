904b12454a1402a3a087d7240cfb65770840da7b
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2021-12-31 18:01:26 -0800

    cleaned up Scoring test by removing comments

--- test/ScoringSpec.hs
+++ test/ScoringSpec.hs
@@ -20,10 +20,6 @@ spec = do
     it "score invalid Game" $ score [] `shouldBe` 0
     it "score zeros" $ score zeroGame `shouldBe` 0
     it "score 1's" $ score onesGame `shouldBe` 20
-   -- it "score perfect game" $ score perfectGame `shouldBe` 300
-
-    --it "overflow" $ plus2 maxBound `shouldBe` minBound + 1
-    --prop "minus 2" $ \i -> plus2 i - 2 `shouldBe` i
 
 makeGameOfFrame :: Frame -> Frame -> Game
 makeGameOfFrame frame lastFrame = 
